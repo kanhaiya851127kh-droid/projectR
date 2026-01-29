1d0c3bbb5d1195193240cbef2e051cc624d4e528
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-01-29 18:00:55 +0530

    add code

---
+++ gitp1.py
@@ -0,0 +1,11 @@
+#box of * type
+n = int(input("Enter the size of square: "))
+
+for i in range(n):
+    for j in range(n):
+        # Print stars only on border
+        if i == 0 or i == n - 1 or -1 or j == 0 or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
