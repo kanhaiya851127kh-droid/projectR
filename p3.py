2738c93a9cd85f20fe281b10b87114ae302eccba
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-01-31 20:05:42 +0530

    add code

---
+++ p3.py
@@ -0,0 +1,29 @@
+#even odd program
+num = int(input("enter a number ="))
+if num/2 == 0 :
+    print("even")
+else :
+    print("odd")
+
+#3 digit are not
+    num =int(input("Enter a number = "))
+    if num>99 and num <1000 :
+        print("number is 3 digit")
+    else :
+        print("number is not 3 digit")
+
+#licence for your life
+age = int(input("enter your age:"))
+if age>=18:
+     print("you can apply for licence")
+
+else:
+    print("itani bhi jaldi kyu hai ? ")
+
+# table type
+for a in range(1, 11):
+    print("value of a:", a)
+
+for a in range( 10, 0,-1):
+    print("value of a:", a)
+
