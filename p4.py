bf947ada986ba2dad9d315de1fc6cd706067fb13
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-02-01 13:09:19 +0530

    add code

---
+++ p4.py
@@ -0,0 +1,14 @@
+
+#even odd
+num = int(input("enter a number ="))
+if num/2 == 0 :
+    print("even")
+else :
+    print("odd")
+
+#vowle consonts
+character = input("enter character =")
+if character in 'aeiouAEUOU':
+    print("vowel")
+else:
+    print("consonts")
