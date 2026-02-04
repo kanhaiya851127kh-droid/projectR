f6e1220ce4798f681eea1e67058bf322d0c6e7fc
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-02-04 19:32:41 +0530

    add code

--- fill in love.py
+++ fill in love.py
@@ -15,17 +15,17 @@ color("red")
 penup()
 
 # text
-goto(0, -200)
+goto(20, -200)
 color("pink")
-write("only s", align="center", font=("Arial", 15, "bold"))
+write("only s", align="center", font=("Arial", 25, "bold"))
 
 color("red")
 
 # heart draw slow
 for i in range(6000):
     k = i * 0.01
-    goto(hearta(k) * 20, heartb(k) * 20)
-    dot(3)
+    goto(hearta(k) * 20, heartb(k) * 19)
+    dot(5)
     time.sleep(0.01)   # increase to 0.02 or 0.03 for more slow
 
 goto(0, 0)
