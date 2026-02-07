9d9c4baadf4d088c5531bf25d2ff1813ffb2df66
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-02-07 22:58:08 +0530

    add code

--- p6.py
+++ p6.py
@@ -1,4 +1,4 @@
-list =[]
+'''list =[]
 for i in range (5):
     n = int(input("enter number "))
     list.append(n)
@@ -25,4 +25,78 @@ for i in lst :
     New =[i]+New
 print(New)
 
+name = ["ram","amit","karan"]
+name.sort()
+print(name)
 
+
+a = [1,2]
+b = [3,4]
+c = a+b
+print(c)
+
+
+lst = [ 1,2,3,4]
+even = [ ]
+odd = [ ]
+for i  in lst :
+    if i % 2 == 0 :
+        even.append(i)
+    else:
+        odd.append(i)
+    print("even :",even)
+   
+    print("odd :",odd)
+
+
+mat = [1,2] , [3,4]
+for row in mat :
+    print(row) 
+
+lst = [10,20,30]
+m = max (lst)
+lst.remove(m)
+print(max(lst))
+
+a = [1,2,3,]
+b = [2,3,4,]
+for i in a :
+    if i in b :
+        print(i)
+
+t = (10,20,30,40,50)
+print( "first element :" , t[0])
+print( "last element :" , t[-1])
+
+
+lst = [ 1,2,3]
+t = tuple(lst)
+
+print("tuple :",t)
+
+
+
+t = [10,20,30]
+print(t.index(30))
+
+t = [10,20,30]
+n = 20
+if n in t :
+    print("presnt")
+else:
+    print ("not present")
+
+t = [2,3,4]
+p = 1
+for i in t :
+    p *= i
+print(p)'''
+
+
+t =  (1,(2,3),4)
+for i in t :
+    if isinstance(i, tuple ):
+        print("nested tuple present")
+        break
+    else:
+        print("not nested")
