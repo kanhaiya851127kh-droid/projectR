4b3ab9ab49a91fa1c59d66e51c335eacaffc2bf5
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-02-17 19:01:40 +0530

    add code

--- unit5.py
+++ unit5.py
@@ -10,3 +10,16 @@ print(apply(lambda n: n*n, 5))
 nums = [1,2,3,4]
 res = list(map(lambda x: x*2, nums))
 print(res)
+
+
+from functools import reduce
+marks=[30,55,80,20,90]
+passed=list(filter(lambda m:m>=40, marks))
+scaled=list(map(lambda m:m+5, passed))
+total=reduce(lambda a,b:a+b, scaled)
+print(total)
+
+
+lst=[[1,2],[3,4]]
+res=list(map(lambda sub:list(map(lambda x:x*2, sub)), lst))
+print(res)
