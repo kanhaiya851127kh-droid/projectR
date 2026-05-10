8eebb0102cb893f80b38d87a541f80998f3bc41e
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-05-10 21:10:36 +0530

    fist code

--- p2.py
+++ p2.py
@@ -1,10 +1,12 @@
-# star in angle
+# star in angle type
+
 rows = int(input("enter number of rows:"))
 for i in range (1, rows + 1):
       print(" " *(rows-1),end = " ") 
       print(" * " * i )
 
-#opse in angle
+#opse in angle type
+
 rows= int(input("enter number of rows :"))
 for i in range(rows , 0 , -1):
     for j in range(i):
