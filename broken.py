1d5c4b0f0099361da7e8e246cbb24d9b0df2a5f7
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-05-21 23:18:04 +0530

    fist code

--- broken.py
+++ broken.py
@@ -22,10 +22,10 @@ def typing(name="GF"):
     print(Fore.YELLOW + f"{name} is typing...")
     time.sleep(2)
 
-# simple dard 
+
 gf_replies = [" ha ", " so what?", "sorry yaar ", "to ky ", "busy hu"]
 
-# chat loop hd
+
 while True:
     user_msg = input("You: ")
     
