fc0eb359dede401991ffa761e1b22a66eee20104
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-07-01 13:05:29 +0530

    fist code

--- love5.py
+++ love5.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-# init
+# -----------------------------------------init------------------------------------------------------
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
@@ -11,7 +11,7 @@ pygame.display.set_caption("Python Particle Animation")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("arial", 40, bold=True)
 
-# ------ PARTICLE CLASS -
+# -------------------------------------- PARTICLE CLASS ---------------------------------------------
 class Particle:
     def __init__(self):
         self.x = random.randint(0, WIDTH)
@@ -31,7 +31,7 @@ class Particle:
             self.size
         )
 
-# MAIN LOOP -----------
+# ----------------------------------------MAIN LOOP ----------------------------------------------------
 particles = []
 running = True
 
