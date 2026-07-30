5cea3060e9d02a08d8d397b41afcdd61c3bb6c0c
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-07-30 20:58:39 +0530

    fist code

--- os.py
+++ os.py
@@ -10,3 +10,12 @@ if guess  == number:
 else:
   print("you lost ! ")
   print("currect number was :",number)
+
+  #include<stdio.h>
+int main() {
+  int currentAge, electionYear;
+  scanf("%d %d", &currentAge, &electionYear);
+  //Age in election year = Current age + (Election year - 2026)
+  currentAge + (electionYear - 2026) >= 18 ? printf("Eligible") : printf("Not Eligible");
+  return 0;
+}
