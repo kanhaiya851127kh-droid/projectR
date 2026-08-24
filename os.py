99b2ed50957b36438e76c37bc010b81aa8192ae4
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-08-24 21:54:50 +0530

    fist code

--- os.py
+++ os.py
@@ -10,3 +10,16 @@ if guess  == number:
 else:
   print("you lost ! ")
   print("currect number was :",number)
+
+import random 
+import os 
+
+number = random.randint(1, 10)
+guess = input("Guess the number between 1 and 10: ")
+guess = int(guess)
+
+if guess  == number:
+    print("you won !")
+else:
+  print("you lost ! ")
+  print("currect number was :",number)
