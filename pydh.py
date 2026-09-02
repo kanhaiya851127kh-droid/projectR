c8edbde68428adbdbf2e09e64e01b5671e4cef8b
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-09-02 14:56:23 +0530

    fist code

---
+++ pydh.py
@@ -0,0 +1,23 @@
+
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+colors = [
+    'orange', 'red', 'magenta', 'blue',
+    'magenta', 'yellow', 'green', 'cyan', 'purple'
+]
+
+s.bgcolor('black')
+t.pensize(2)
+t.speed(0)
+
+for x in range(360):
+    t.pencolor(colors[x % 9])
+    t.width(x // 100 + 1)
+    t.forward(x)
+    t.right(59)
+
+turtle.hideturtle()
+turtle.done()
