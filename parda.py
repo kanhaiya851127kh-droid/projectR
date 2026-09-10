3c847a8be2941fe88b9d234ae15be5f60829f68d
kanhaiya__1214__ZX <kanhaiya851127kh@gmail.com>
2026-09-10 21:49:46 +0530

    fist code

--- parda.py
+++ parda.py
@@ -12,3 +12,35 @@ int open_link(const char *url) {
     system(command);
 #elif __APPLE__
     char command[512];
+rintf("Opening link: %s\n", url);
+    
+#ifdef _WIN32
+    char command[512];
+    snprintf(command, sizeof(command), "start \"\" \"%s\"", url);
+    system(command);
+#elif __APPLE__
+    char command[512];
+rintf("Opening link: %s\n", url);
+    
+#ifdef _WIN32
+    char command[512];
+    snprintf(command, sizeof(command), "start \"\" \"%s\"", url);
+    system(command);
+#elif __APPLE__
+    char command[512];
+rintf("Opening link: %s\n", url);
+    
+#ifdef _WIN32
+    char command[512];
+    snprintf(command, sizeof(command), "start \"\" \"%s\"", url);
+    system(command);
+#elif __APPLE__
+    char command[512];
+rintf("Opening link: %s\n", url);
+    
+#ifdef _WIN32
+    char command[512];
+    snprintf(command, sizeof(command), "start \"\" \"%s\"", url);
+    system(command);
+#elif __APPLE__
+    char command[512];
